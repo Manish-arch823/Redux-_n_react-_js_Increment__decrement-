191d388d51a4108d754e6a7d675a161ff12a9c16
Manish <manishsinghsemra2@gmail.com>
2021-08-15 20:21:51 +0530

    Redux-_n_react-_js_Increment__decrement

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import {incNumber,decNumber } from "./actions/index";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+
+
+const App = () => {
+  const myState = useSelector((state) => state.changeTheNumber)
+  const dispatch = useDispatch();
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" >
+      <h1>Increment / Decrement</h1>
+      <h4>Using React and Redux</h4>
+      <div className="quentity" >
+        <a className="quentity_minus"   title="Decrement" onClick={() => dispatch(decNumber())} ><span type="button" class="btn btn-danger">-</span></a>
+        <span class="jumbotron" >
+         <span style={{borderStyle:"none",paddingLeft:"50px" ,paddingRight:"50px",fontSize:"50px"}} type="text" className="quentity_input" >{myState}</span>
+        </span>
+      
+        <a className="quentity_plus" title="Increment" onClick={ () => dispatch(incNumber(5))} ><span type="button" class="btn btn-success" >+</span></a>
+      </div>
+      
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import changeTheNumber from "./upDown";
+
+import {combineReducers} from "redux";
+
+
+const rootReducer = combineReducers({
+    changeTheNumber,
+    // changeBackgroundColor,
+})
+
+export default  rootReducer
